f491c8ba10df3de47f5b5041fd518d77a8267f36
Rahulkalal <107499198+Rahulkalal@users.noreply.github.com>
2024-12-29 21:02:37 +0530

    Update main.tf

--- import/main.tf
+++ import/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "hands_on_hcl" {
+resource "aws_instance" "K8s_hands_on" {
   # You can leave the resource block empty for now or add some basic attributes later
   # Placeholder for the EC2 instance.
   # Add required fields like `ami` and `instance_type` after the import
