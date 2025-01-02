a19704964d0aab69a4738c6a6a9b324071573a31
Rahulkalal <107499198+Rahulkalal@users.noreply.github.com>
2025-01-02 11:46:08 +0530

    Update backend.tf

--- import/backend.tf
+++ import/backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket-hcl"       # Replace with your S3 bucket name
-    key            = "terraform/state.tfstate"   # Path where the state file will be stored in the S3 bucket
+    key            = "import/terraform/state.tfstate"   # Path where the state file will be stored in the S3 bucket
     region         = "us-east-1"                 # Region of your S3 bucket (adjust as needed)
     dynamodb_table = "dynamodb-lock-table-hcl1"  # Replace with your DynamoDB table name for state locking
     encrypt        = true                        # Ensure the state file is encrypted in S3
