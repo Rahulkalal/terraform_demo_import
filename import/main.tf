70404c108947249bdaa60fca40de3b2bdb092138
Rahulkalal <107499198+Rahulkalal@users.noreply.github.com>
2025-01-20 20:31:59 +0530

    Update main.tf

--- import/main.tf
+++ import/main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "Terraform-Demo-import" {
+resource "aws_instance" "test_import" {
   # You can leave the resource block empty for now or add some basic attributes later
   # Placeholder for the EC2 instance.
   # Add required fields like `ami` and `instance_type` after the import
-  ami           = "ami-0e2c8caa4b6378d8c"  # Replace with actual AMI ID
+  ami           = "ami-09e143e99e8fa74f9"  # Replace with actual AMI ID
   instance_type = "t2.micro"              # Replace with actual instance type
 }
