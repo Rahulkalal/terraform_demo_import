9f980f6d62b331deeff7f47a7a5c187a9f212192
Rahulkalal <107499198+Rahulkalal@users.noreply.github.com>
2025-01-21 14:19:26 +0530

    Update main.tf

--- import/main.tf
+++ import/main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
-resource "aws_instance" "test_import" {
+resource "aws_instance" "tets_import" {
   # You can leave the resource block empty for now or add some basic attributes later
   # Placeholder for the EC2 instance.
   # Add required fields like `ami` and `instance_type` after the import
-  ami           = "ami-09e143e99e8fa74f9"  # Replace with actual AMI ID
+  ami           = "ami-0d11f9bfe33cfbe8b"  # Replace with actual AMI ID
   instance_type = "t2.micro"              # Replace with actual instance type
 }
